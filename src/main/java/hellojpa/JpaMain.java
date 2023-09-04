778334173b19ba76a5720765cb63ded80406e9ce
pysdsc <pysdsc@gmail.com>
2023-09-04 23:50:44 +0900

    영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,10 +16,10 @@ public class JpaMain {
         try {
 
             //영속
-            Member findMember1 = em.find(Member.class, 101l);
-            Member findMember2 = em.find(Member.class, 101l);
+            Member member = em.find(Member.class, 150L);
+            member.setName("zzz");
 
-            System.out.println("result = "+ (findMember1 == findMember2));
+            System.out.println("========================");
 
             tx.commit();
         } catch (Exception e) {
