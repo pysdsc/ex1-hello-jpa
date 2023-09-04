b439dae242175747eec91bb4cea6e08bbd7f7065
pysdsc <pysdsc@gmail.com>
2023-09-04 23:56:50 +0900

    영속성 컨텍스트 2

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -10,6 +10,10 @@ public class Member {
     private Long id;
     private String name;
 
+    public Member() {
+
+    }
+
     public Member(Long id, String name) {
         this.id = id;
         this.name = name;
